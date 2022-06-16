213e7b1d799f2d2e1c5b71f7e6154902a273cb64
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-06-16 01:32:22 -0400

    [x86-calc] reversing strings successfully

--- x86-calc/x86-calc.asm
+++ x86-calc/x86-calc.asm
@@ -3,6 +3,9 @@
 ; Addition calculator program.
 ;
 ; CHANGELOG :
+;   v1.0.0 - 2022-06-16t01:29Q
+;       reversing strings successfully
+;
 ;   v0.1.0 - 2022-06-15t22:00Q
 ;       counters for reverse push, pop loops
 ;
@@ -28,6 +31,10 @@ _start:
     mov rdi, 1          ; file descriptor to which to print, namely:
                         ; STDOUT (standard output)
     syscall     ; execute the system call
+    ; C equivalent: write(1, ENDL, 1);
+    mov rsi, ENDL       ; newline to print
+    mov rdx, 1          ; 1 character to print
+    syscall     ; execute the system call
 ; label for end of the program
 END:
     ; C equivalent: exit(EXIT_SUCCESS);
@@ -42,19 +49,27 @@ END:
 ; of the original string.
 STRREV:
     push rcx            ; backup counter
+    push r8             ; backup general purpose r8 for string address
+    push r9             ; backup general purpose r9 for character
     mov rcx,rdx         ; set counter to rdx
+    mov r8,rsi          ; initialize the 
 ; push loop
 STRREV_PUSH_LOOP:
-    dec rcx                 ; count down
-    cmp rcx,0               ; if (rcx==0)
-    jne STRREV_PUSH_LOOP    ; then repeat
+    mov r9,[r8]             ; copy the character
+    push r9                 ; push it onto stack
+    inc r8                  ; next character
+    loop STRREV_PUSH_LOOP   ; repeat
 STRREV_PUSH_END:
     mov rcx,rdx         ; set counter to rdx
+    mov r8,rsi          ; initialize the 
 STRREV_POP_LOOP:
-    dec rcx                 ; count down
-    cmp rcx,0               ; if (rcx==0)
-    jne STRREV_POP_LOOP    ; then repeat
+    pop r9                  ; pop the character
+    mov [r8],r9             ; place the character in the string
+    inc r8                  ; next character
+    loop STRREV_POP_LOOP    ; repeat
 STRREV_POP_END:
+    pop r9              ; restore general purpose
+    pop r8              ; restore general purpose
     pop rcx             ; restore counter
     ret
 ; end STRREV
@@ -65,7 +80,9 @@ STRREV_POP_END:
 section .data
 
 ; string to be reversed
-REV_TEST:     db "Hello world!", 0ah
+REV_TEST:       db "Hello world!"
 ; length of REV_TEST
 REV_LEN:        equ $ - REV_TEST
+; newline character
+ENDL:           db 0ah
 
