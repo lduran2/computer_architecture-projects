498753d7c851825ab4ab9d0efc4c0b8b47e1b7d5
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-10-16 00:24:17 -0400

    [verilog-ired] finished ir_emitter datapath

--- verilog-ir_emitter_datapath/ir_emitter_dp.sv
+++ verilog-ir_emitter_datapath/ir_emitter_dp.sv
@@ -43,15 +43,31 @@ module ir_emitter_dp (
 	// corresponding bit size
 	parameter BIT_SIZE_2F = 11;
 
+	// counts corresponding to the Lego RC Encoding specification
+	// since counts are in from mod_init_count down to 0, these counts
+	// mark (mod_init_count + 1) cycles.
+	//  6 cycles for IR mark
+	parameter IR_MARK = 6'd5;
+	// 10 cycles for Low bit pause
+	parameter LOW_BIT_PAUSE = 6'd9;
+	// 21 cycles for High bit pause
+	parameter HIGH_BIT_PAUSE = 6'd20;
+	// 39 cycles for Start/Stop bit pause
+	parameter START_STOP_BIT_PAUSE = 6'd38;
+	// the corresponding bit size
+	parameter MOD_BIT_SIZE = 6;
+
 	// terminal count of 76k divider
 	wire tc_2f;
 	// carrier signal produced by u_sw_carrier
 	wire sw_carrier;
 	// terminal count produced by u_sw_carrier
 	wire tc_carrier;
+	// initial count for the modulator
+	reg [5:0] mod_init_count;
 
 	// the 2f divider
-	// the count is ignored
+	// ignore the count
 	divider #(.BIT_SIZE(BIT_SIZE_2F)) u_2f(
 		// output
 		.tc(tc_modulator), .count(),
@@ -69,5 +85,27 @@ module ir_emitter_dp (
 
 	// short circuit emitter_out if not sw_modulator
 	and emitter_and(emitter_out, sw_modulator, sw_carrier);
+
+	always @* begin
+		// 4x1 mux
+		// choose the count depending on mod_sel and
+		// the Lego RC Encoding specification
+		case (mod_sel)
+			2'b00: mod_init_count = IR_MARK;
+			2'b01: mod_init_count = LOW_BIT_PAUSE;
+			2'b10: mod_init_count = HIGH_BIT_PAUSE;
+			2'b11: mod_init_count = START_STOP_BIT_PAUSE;
+			default: mod_init_count = 6'd0;
+		endcase // (mod_sel)
+	end // always @*
+
+	// the modulator divider
+	// ignore the count
+	divider #(.BIT_SIZE(MOD_BIT_SIZE)) u_modulator(
+		// output
+		.tc(tc_modulator), .count(),
+		// input
+		.init_count(mod_init_count), .clk(clk), .rst(rst), .ena(tc_carrier)
+	);
 
 endmodule // ir_emitter_dp
