a4a89f5ae9d69b1331be25cbd75b921d7d89f4e3
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-10-15 22:54:27 -0400

    [verilog-ired] 2f divider

--- verilog-ir_emitter_datapath/ir_emitter_dp.sv
+++ verilog-ir_emitter_datapath/ir_emitter_dp.sv
@@ -6,15 +6,23 @@
 // synthesis >= Yosys 0.9.0
 //
 //////////////////////////////////////////////////////////////////////////////////
-// Data path that creates a modulated 38 kHz carrier wave for an
+// Data path that creates a modulated f = 38 kHz carrier wave for an
 // infrared wave from the Basys3's 100 MHz clock input.
+//
+// The output signal should be a square wave with a 50% duty cycle. 
+// Thus, the clock signal is first divided into a
+// 2f = 2(38 kHz) = 76 kHz signal by a 1316-count divider (because
+// 1316*76 kHz = 100.016 MHz, the lowest multiple of 76 kHz greater
+// than the original clock frequency of 100 MHz) before being divided
+// into the 2-state duty cycle by a 2-count divider giving the desired
+// duty cycle.
 //
 // Output:
 //     emitter_out: wire = modulated clock signal driving
 //         the IR emitter
 //     tc_modulator: wire = signals that modulation count completed
 // Input:
-//     clk: wire = Basys3 system clock signal
+//     clk: wire = the Basys3 system clock signal
 //     rst: wire = signal to force reset all internal dividers
 //     ena: wire = enable signal for carrier frequency counter
 //     mod_sel: wire [1:0] = selects the initial count for the modulator
@@ -28,6 +36,23 @@ module ir_emitter_dp (
 	input wire ena,
 	input wire [1:0] mod_sel,
 	input wire sw_modulator
+	);
+
+	// count for dividing the system clock signal to 2f
+	parameter COUNT_2F = 11'd1315;
+	// corresponding bit size
+	parameter BIT_SIZE_2F = 11;
+
+	// terminal count of 76k divider
+	wire tc_2f;
+
+	// the 2f divider
+	// the count is ignored
+	divider #(.BIT_SIZE(BIT_SIZE_2F)) u_2f(
+		// output
+		.tc(tc_modulator), .count(),
+		// input
+		.init_count(COUNT_2F), .clk(clk), .rst(rst), .ena(ena)
 	);
 
 endmodule // ir_emitter_dp
